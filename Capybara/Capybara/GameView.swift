cbd7fd2c5ad6d77fd4bef65ad1480b0eb785b802
Daniil Pazin <pazin.d@icloud.com>
2023-09-07 12:49:14 +0200

    Tab bar added.

---
+++ Capybara/Capybara/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  Capybara
+//
+//  Created by Пазин Даниил on 07/09/2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
---
+++ Capybara/Capybara/PizzaView.swift
@@ -0,0 +1,20 @@
+//
+//  PizzaView.swift
+//  Capybara
+//
+//  Created by Пазин Даниил on 07/09/2023.
+//
+
+import SwiftUI
+
+struct PizzaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PizzaView_Previews: PreviewProvider {
+    static var previews: some View {
+        PizzaView()
+    }
+}
