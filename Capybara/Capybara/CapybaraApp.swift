5e95ea9cb368a79960a620bd8c5a62b59ebba5d2
Daniil Pazin <pazin.d@icloud.com>
2023-09-07 12:35:55 +0200

    App created.

---
+++ Capybara/Capybara/CapybaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapybaraApp.swift
+//  Capybara
+//
+//  Created by Пазин Даниил on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CapybaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
