9bcce7ad8d4c8451556cb79debefa6765bf01511
Mateusz Czechowski <maticzech@icloud.com>
2023-09-07 16:43:51 +0200

    merged

--- Capybara/Capybara/View/ProjectView.swift
+++ Capybara/Capybara/View/ProjectView.swift
@@ -15,29 +15,28 @@ struct ProjectView: View {
     var body: some View {
 
         NavigationStack {
-            List {
-                Text("Some project should be here.")
-                Text("Some another project should be here.")
+            TabView {
+                CategoryView()
+                CategoryView()
+                CategoryView()
+                CategoryView()
             }
+            .tabViewStyle(.page)
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            .toolbarBackground(.clear, for: .navigationBar)
             .toolbar {
                 Button {
                     print("Adding project logic.")
-                    let ref = Database.database(url: "https://capybara-d1f5f-default-rtdb.europe-west1.firebasedatabase.app").reference()
-                    ref.child("category").getData(completion:  { error, snapshot in
-                        guard error == nil else {
-                            print(error!.localizedDescription)
-                            return;
-                        }
-                        let test = snapshot?.value
-                        print("@@@ TEST: ", test)
-                    })
+                    
                     self.isPresented = false
                 } label: {
-                    Image(systemName: "plus")
+                    Text("Close")
                 }
-
             }
         }
+//        NavigationStack {
+//            Text("Category name")
+//        }
 
 //        .navigationBarTitleDisplayMode(.automatic)
     }
